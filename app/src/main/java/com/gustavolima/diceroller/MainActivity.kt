2c014033eca7f6359736413edede82ffb0da7b95
Gustavo Lima <gustavo.f12@hotmail.com>
2023-07-28 16:43:12 -0300

    Logic to generate random Int has been implemented.

--- app/src/main/java/com/gustavolima/diceroller/MainActivity.kt
+++ app/src/main/java/com/gustavolima/diceroller/MainActivity.kt
@@ -2,10 +2,33 @@ package com.gustavolima.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.gustavolima.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+     //   val roolButton: Button = binding.roolButton
+       binding.roolButton.setOnClickListener {
+           roolDice()
+
+       }
+
+
+
+
     }
+
+    private fun roolDice() {
+    val randromInt = Random.nextInt(6) + 1
+        binding.textView.text = randromInt.toString()
+    }
+
+
 }
