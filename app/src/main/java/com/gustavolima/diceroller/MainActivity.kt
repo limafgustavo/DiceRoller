239f185512dac8950f41001e75b9883db95bb7e2
Gustavo Lima <gustavo.f12@hotmail.com>
2023-08-07 20:42:29 -0300

    Added two dices.

--- app/src/main/java/com/gustavolima/diceroller/MainActivity.kt
+++ app/src/main/java/com/gustavolima/diceroller/MainActivity.kt
@@ -14,20 +14,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-     //   val roolButton: Button = binding.roolButton
-       binding.roolButton.setOnClickListener {
-           roolDice()
 
-       }
+        startEmptyDice()
 
+        binding.roolButton.setOnClickListener {
+            roolDice()
 
+        }
 
 
     }
 
+    private fun startEmptyDice() {
+        binding.imageDice1.setBackgroundResource(R.drawable.empty_dice)
+        binding.imageDice2.setBackgroundResource(R.drawable.empty_dice)
+    }
+
     private fun roolDice() {
-    val randromInt = Random.nextInt(6) + 1
-        binding.textView.text = randromInt.toString()
+        val randomInt1 = Random.nextInt(6)+1
+        val randomInt2 = Random.nextInt(6)+1
+
+        val diceImages = arrayOf(
+        R.drawable.empty_dice,
+            R.drawable.dice_1,
+            R.drawable.dice_2,
+            R.drawable.dice_3,
+            R.drawable.dice_4,
+            R.drawable.dice_5,
+            R.drawable.dice_6
+        )
+
+        binding.imageDice1.setBackgroundResource(diceImages[randomInt1])
+        binding.imageDice2.setBackgroundResource(diceImages[randomInt2])
+
     }
 
 
